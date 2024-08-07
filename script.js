b7b20f4ca5d50ced6c5363e11a6655031c0886af
Sathish <142983325+sathish3771@users.noreply.github.com>
2024-08-07 17:29:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-let btn=document.getElementById('NavBtn')
-btn.addEventListener("click",function(){
-    window.location.href="SignUpPage3.html";
-});
-
-function NavBtn1(){
-window.location.href="landingPage.html";
-}
-document.addEventListener("DOMContentLoaded",function(){
-    let btn2=document.getElementById("btnNext");
-    if(btn2){
-        btn2.addEventListener("click",NavBtn1);
+document.addEventListener("DOMContentLoaded", function() {
+    let btn = document.getElementById('NavBtn');
+    if (btn) {
+        btn.addEventListener("click", function() {
+            window.location.href = "SignUpPage3.html";
+        });
     }
-})
+
+    let btn2 = document.getElementById("btnNext");
+    if (btn2) {
+        btn2.addEventListener("click", function() {
+            window.location.href = "landingPage.html";
+        });
+    }
+});
